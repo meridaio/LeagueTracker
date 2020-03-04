5ebb62881020bf3c97296a782b32a2e10973976b
Nathan <nathan@paulgeorge.com>
2020-03-03 23:50:48 -0500

    initial commit -- prints game events to console

---
+++ LeagueTracker/LeagueClient.fs
@@ -0,0 +1,56 @@
+﻿module LeagueClient
+
+open RestSharp
+open FSharp.Data
+open System.Net.Security
+
+let client =
+    let c = RestClient("https://127.0.0.1:2999")
+    c.RemoteCertificateValidationCallback <- RemoteCertificateValidationCallback(fun _ _ _ _ -> true)
+    c
+
+type ScoreJson = JsonProvider<"""
+{
+    "assists": 0,
+    "creepScore": 0,
+    "deaths": 0,
+    "kills": 0,
+    "wardScore": 0.0
+}
+""">
+
+type AllJson = JsonProvider<"https://static.developer.riotgames.com/docs/lol/liveclientdata_sample.json">
+
+type EventJson = JsonProvider<"https://static.developer.riotgames.com/docs/lol/liveclientdata_events.json">
+
+let getAllStats () =
+    async {
+        let req = RestRequest "liveclientdata/allgamedata"
+        let! res = Async.AwaitTask <| client.ExecuteAsync req
+        let content = res.Content
+        return AllJson.Parse content
+    }
+
+let getPlayerName () =
+    async {
+        let req = RestRequest "liveclientdata/activeplayername"
+        let! res = Async.AwaitTask <| client.ExecuteAsync<string> req
+        return res.Data
+    }
+
+let getPlayerScore player =
+    async {
+        let req = RestRequest("liveclientdata/playerscores").AddQueryParameter("summonerName", player)
+        let! res = Async.AwaitTask <| client.ExecuteAsync req
+        return ScoreJson.Parse res.Content
+    }
+
+let getEvents () =
+    async {
+        try 
+            let req = RestRequest "liveclientdata/eventdata"
+            let! res = Async.AwaitTask <| client.ExecuteAsync req
+            return Some <| EventJson.Parse res.Content
+        with
+        | _ -> return None
+    }
---
+++ LeagueTracker/Program.fs
@@ -0,0 +1,55 @@
+﻿open System
+
+open LeagueClient
+open System.Net.NetworkInformation
+
+let printWelcome (allInfo: AllJson.Root) =
+    printfn "Hello, %s! Welcome to League Tracker!" allInfo.ActivePlayer.SummonerName
+    printfn "Your current score is: %A" (Array.find (fun (x: AllJson.AllPlayer) -> x.SummonerName = allInfo.ActivePlayer.SummonerName) allInfo.AllPlayers).SummonerName
+    printfn "All events: %A" allInfo.Events.Events
+
+let rec loop (name: string) (callback: EventJson.Root -> Async<unit>) =
+    async {
+        let! events = getEvents ()
+        match events with
+        | Some e -> 
+            do! callback e
+            do! Async.Sleep 500
+            return! loop name callback
+        | None ->
+            return ()
+    }
+
+let rec waitForGame () = 
+    async {
+        let gameRunning = 
+            IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpListeners()
+            |> Array.exists (fun x -> x.Port = 2999)
+
+        if gameRunning then
+            printfn "Game started!"
+            return ()
+        else 
+            printfn "Game not started, waiting..."
+            do! Async.Sleep 1000
+            return! waitForGame ()
+    }
+
+let processEvents (events: EventJson.Root) =
+    async {
+        return printfn "Events: %A" events
+    }
+
+let rec programLoop () =
+    async {
+        do! waitForGame ()
+        let! allInfo = getAllStats ()
+        printWelcome allInfo
+        do! loop allInfo.ActivePlayer.SummonerName processEvents
+        return! programLoop ()
+    }
+
+[<EntryPoint>]
+let main argv =
+    programLoop () |> Async.RunSynchronously
+    0
